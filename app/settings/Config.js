4b5bfe34b51c7e8a0a4ca9697b5b3921fb3a955f
Jamal Hassouni <ja.hassouni@gmail.com>
2021-05-25 22:02:00 +0100

    fix:Fix Video Link not working on Production

--- app/settings/Config.js
+++ app/settings/Config.js
@@ -1,7 +1,7 @@
 export const API_URL = __DEV__
-  ? "https://tamrini.com/api"
+  ? "http://localhost:5000/api"
   : "https://tamrini.com/api";
 
 export const YOUTUBE_PLAYER = __DEV__
   ? "https://tamrini.com/play-youtube?videoId="
-  : "https://tamrini.com/api/play-youtube?videoId=";
+  : "https://tamrini.com/play-youtube?videoId=";
